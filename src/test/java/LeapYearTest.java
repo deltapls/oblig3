47e515f1982c1a5c97d0a9ad715c9348adbd3282
Eirik Skoglund <89611826+deltapls@users.noreply.github.com>
2021-11-04 16:49:56 +0100

    Added oblig2 project

---
+++ src/test/java/LeapYearTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LeapYearTest {
+
+    @Test
+    public void DivisibleBy4not100() {
+        Assertions.assertEquals(true, LeapYear.isLeapYear(1940));
+    }
+
+    @Test
+    public void NotDivisibleBy4() {
+        Assertions.assertEquals(false, LeapYear.isLeapYear(1914));
+    }
+
+    @Test
+    public void DivisibleBy400() {
+        Assertions.assertEquals(true, LeapYear.isLeapYear(2000));
+    }
+
+    @Test
+    public void DivisibleBy100not400() {
+        Assertions.assertEquals(false, LeapYear.isLeapYear(2100));
+    }
+}
